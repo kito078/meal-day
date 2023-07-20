1839587abef1c2adf05549ef47d05741026a8d2e
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-20 16:00:43 +0200

    ind

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,91 +1,146 @@
-import React, { useState } from "react";
+import React from "react";
+import "./Navbar.css";
 
 function Navbar() {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleHandler = () => {
-    setIsOpen(!isOpen);
-  };
   return (
-    <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" class="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
-        </a>
-        <button
-          onClick={toggleHandler}
-          data-collapse-toggle="navbar-solid-bg"
-          type="button"
-          class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-solid-bg"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div
-          class={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto"`}
-          id="navbar-solid-bg"
-        >
-          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+    <nav className="container relative mx-auto p-6">
+      {/* flex container for Nav Items */}
+      <div className="flex items-center justify-between space-x-20 my-6">
+        {/* logo */}
+        <div className="z-30">
+          <h5 id="logo">MealsDay</h5>
         </div>
+        {/* Menu Items */}
+        <div className="hidden items-center space-x-10 uppercase text-grayisBlue md:flex">
+          <a href="#home" className="tracking-widest hover:text-softRed">
+            Home
+          </a>
+          <a href="#about" className="tracking-widest hover:text-softRed">
+            About
+          </a>
+          <a href="#services" className="tracking-widest hover:text-softRed">
+            Services
+          </a>
+          <a
+            href=""
+            className="px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md hover:text-softRed hover:bg-white
+          "
+          >
+            download
+          </a>
+        </div>
+        {/* hamburger Button */}
+        <button
+          id="menu-btn"
+          className="z-30 block md:hidden focus:outline-none hamburger"
+        >
+          {/* humburger lines */}
+          <span className="hamburger-top"></span>
+          <span className="hamburger-middle"></span>
+          <span className="hamburger-bottom"></span>
+        </button>
+      </div>
+      {/* Mobile Menu */}
+      <div
+        id="menu"
+        className="fixed inset-0 z-20 hidden flex-col items-center self-end w-full h-full m-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y  divide-gray-500 opacity-90 bg-veryDarkBlue"
+      >
+        hello
       </div>
     </nav>
   );
 }
 
 export default Navbar;
+
+// import React, { useState } from "react";
+
+// function Navbar() {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const toggleHandler = () => {
+//     setIsOpen(!isOpen);
+//   };
+//   return (
+//     <nav class="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
+//       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+//         <a href="#" class="flex items-center">
+//           <img
+//             src="https://flowbite.com/docs/images/logo.svg"
+//             class="h-8 mr-3"
+//             alt="Flowbite Logo"
+//           />
+//           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+//             Flowbite
+//           </span>
+//         </a>
+//         <button
+//           onClick={toggleHandler}
+//           data-collapse-toggle="navbar-solid-bg"
+//           type="button"
+//           class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+//           aria-controls="navbar-solid-bg"
+//           aria-expanded="false"
+//         >
+//           <span class="sr-only">Open main menu</span>
+//           <svg
+//             class="w-5 h-5"
+//             aria-hidden="true"
+//             xmlns="http://www.w3.org/2000/svg"
+//             fill="none"
+//             viewBox="0 0 17 14"
+//           >
+//             <path
+//               stroke="currentColor"
+//               stroke-linecap="round"
+//               stroke-linejoin="round"
+//               stroke-width="2"
+//               d="M1 1h15M1 7h15M1 13h15"
+//             />
+//           </svg>
+//         </button>
+//         <div
+//           class={`${isOpen ? "block" : "hidden"} w-full md:block md:w-auto"`}
+//           id="navbar-solid-bg"
+//         >
+//           <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+//             <li>
+//               <a
+//                 href="#"
+//                 class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+//                 aria-current="page"
+//               >
+//                 Home
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 href="#"
+//                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//               >
+//                 Services
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 href="#"
+//                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//               >
+//                 Pricing
+//               </a>
+//             </li>
+//             <li>
+//               <a
+//                 href="#"
+//                 class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//               >
+//                 Contact
+//               </a>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default Navbar;
