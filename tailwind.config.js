d439b317b59c487bc5cf694f3920a30213bbd8bb
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-21 02:07:13 +0200

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
     },
     extend: {
       colors: {
-        softBlue: "hsl(231, 69%, 60%)",
-        softRed: "hsl(0, 94%, 66%)",
+        softBlue: "#2bbef9",
+        softRed: "#ed174a",
         grayishBlue: "hsl(229, 8%, 60%)",
         veryDarkBlue: "hsl(226, 31%, 21%)",
       },
