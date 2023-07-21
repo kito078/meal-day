73c9578642c2be9fefc4acae42153704b2c3f47a
kito078 <101482292+kito078@users.noreply.github.com>
2023-07-21 04:41:03 +0200

    meal

--- src/components/meal/MealItem.js
+++ src/components/meal/MealItem.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./MealItem.css";
 
 function MealItem({ mealData }) {
   const { strCategory, strMealThumb } = mealData;
   return (
-    <div>
-      <div className="max-w-sm rounded-3xl shadow-lg">
+    <div className="rounded-extra ">
+      <div className=" max-w-sm rounded-3xl shadow-lg">
         <img className="w-full rounded-3xl" src={strMealThumb} alt="" />
       </div>
       <h3>{strCategory}</h3>
