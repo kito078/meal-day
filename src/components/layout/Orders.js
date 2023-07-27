aa8f8ad57c9feaf0e7d0f2c55d53c86977112ee5
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-07-27 14:39:48 +0200

    order

--- src/components/layout/Orders.js
+++ src/components/layout/Orders.js
@@ -31,6 +31,12 @@ const items = [
 function Orders() {
   return (
     <div className="bg-softBlueWhite">
+      <div className="container mx-auto text-center my-32">
+        <h1 class="mb-6 text-5xl text-slate-700 uppercase font-semibold text-center leading-tight md:text-6xl lg:text-7xl">
+          How to Order?
+        </h1>
+        <p className="text-slate-500">Follow the Steps</p>
+      </div>
       <div className=" mx-auto text-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 ">
         {items.map((item) => (
           <Order item={item} />
