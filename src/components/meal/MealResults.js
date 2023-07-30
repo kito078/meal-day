4c88202985c41697c348a6b4605b6355747bd000
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-07-30 15:11:40 +0200

    good

--- src/components/meal/MealResults.js
+++ src/components/meal/MealResults.js
@@ -1,9 +1,7 @@
 import React, { useContext, useEffect, useState } from "react";
 import { PuffLoader } from "react-spinners";
-
 import MealItem from "./MealItem";
 import MealContext from "../../hooks/MealContext";
-import SearchMeal from "./SearchMeal";
 import { fetchMeal } from "../../hooks/MealAction";
 
 function MealResults() {
@@ -27,7 +25,7 @@ function MealResults() {
     return (
       <div className=" mx-auto">
         <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center">
-          {meals.map((mealData) => (
+          {mapedMeals.map((mealData) => (
             <MealItem key={mealData.idMeal} mealData={mealData} />
           ))}
         </div>
